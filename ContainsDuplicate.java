41e4265878af2ab3a752a85b58b0fb2cd8680977
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-03-23 19:08:18 +0530

    217. Contains Duplicate

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -11,3 +11,17 @@ class Solution {
         return false;
     }
 }
+
+////////////////// Using Set
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<nums.length; i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
