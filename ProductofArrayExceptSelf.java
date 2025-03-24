ce09c7d440ab7b3aa14c5a41ff8df2c5a5122d94
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-03-24 20:10:12 +0530

    3169. Count Days Without Meetings (4)

--- ProductofArrayExceptSelf.java
+++ ProductofArrayExceptSelf.java
@@ -19,5 +19,21 @@ class Solution {
             ans[i] = left[i] * right[i];
         }
         return ans;
+
+
+         // Optimal Solution********************************************
+        int ans[] = new int[n];
+        int prefix = 1;
+        for(int i = 0; i<n; i++){
+            ans[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int postfix = 1;
+        for(int i = n-1; i>=0; i--){
+            ans[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return ans;
     }
 }
