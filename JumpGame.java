21bf0f6b82b2fa8033f4a3d87d5f3d988fb08e8b
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-04-07 23:36:24 +0530

    55. Jump Game

---
+++ JumpGame.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int finall = nums.length-1;
+
+        for(int i = nums.length-2; i>=0; i--){
+            if(i+nums[i] >= finall){
+                finall = i;
+            }
+        }
+        return (finall == 0) ? true : false;
+    }
+}
