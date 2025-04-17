e3cf89a9de485e18c483597b1df6b52b5828c8a7
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-04-18 00:58:20 +0530

    19. Remove Nth Node From End of List

--- RemoveNthNodeFromEndofList.java
+++ RemoveNthNodeFromEndofList.java
@@ -1,23 +1,42 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        // if(head == null) return null;
+        // ListNode curr = head;
+        // int length = 0;
+        // while(curr != null){
+        //     length++;
+        //     curr = curr.next;
+        // }
+        // int del = length - n + 1;
+        // if(del == 1) return head.next;
+        // ListNode temp = head;
+        // int count = 1;
+        // while(count< del -1){
+        //     temp = temp.next;
+        //     count++;
+        // }
+        // if(temp.next != null){
+        //     temp.next = temp.next.next;
+        // }
+        // return head;
+
+
+         // Using two pointer approach
         if(head == null) return null;
-        ListNode curr = head;
-        int length = 0;
-        while(curr != null){
-            length++;
-            curr = curr.next;
+
+        ListNode back = head;
+        ListNode front = head;
+        for(int i = 0; i<n; i++){
+            front = front.next;
         }
-        int del = length - n + 1;
-        if(del == 1) return head.next;
-        ListNode temp = head;
-        int count = 1;
-        while(count< del -1){
-            temp = temp.next;
-            count++;
+
+        if(front == null) return head.next;
+        while(front.next != null){
+            back = back.next;
+            front= front.next;
         }
-        if(temp.next != null){
-            temp.next = temp.next.next;
-        }
+         
+        back.next = back.next.next;
         return head;
     }
 }
