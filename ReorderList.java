cdb524eb92fee72c2163726c84da8ea32b7567a7
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-04-19 01:54:03 +0530

    143. Reorder List

---
+++ ReorderList.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+        Stack<ListNode> st = new Stack<>();
+        ListNode curr = head;
+        int len = 0;
+        while(curr != null){
+            st.push(curr);
+            curr = curr.next;
+            len++;
+        }
+        curr = head;
+        for(int i = 0; i < len/2; i++){
+            ListNode last = st.pop();
+            last.next = curr.next;
+            curr.next = last;
+            curr = last.next;
+        }
+        if(curr != null){
+            curr.next = null;
+        }
+
+    }
+}
